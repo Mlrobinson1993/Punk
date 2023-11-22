f41f70b2575804fc7f8fb49bc0b0720c08edbb5d
Mike Robinson <michael.robinson@pertemps.co.uk>
2023-11-22 20:27:31 +0000

    update base url

--- Punk.Frontend/nuxt.config.ts
+++ Punk.Frontend/nuxt.config.ts
@@ -1,24 +1,12 @@
-console.log("processing for: " + process.env.BASE_URL);
-
 export default defineNuxtConfig({
   ssr: false,
   modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", "nuxt-svgo"],
   runtimeConfig: {
     public: {
-      apiBase: process.env.BASE_URL,
+      apiBase: "https://punk-api.azurewebsites.net",
     },
   },
 
-  // nitro: {
-  //   devProxy: {
-  //     "/api/": {
-  //       target: process.env.BASE_URL,
-  //       changeOrigin: true,
-  //       prependPath: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
   svgo: {
     defaultImport: "component",
     componentPrefix: "Icon",
